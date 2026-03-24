f4bcba02e96df70191b6b40320a4930f26fadb15
Charles Chang <42170907+CharlesVoyager@users.noreply.github.com>
2026-03-24 21:12:32 +0800

    Clean code.

--- ModelLib/model/ThreeDModel.cs
+++ ModelLib/model/ThreeDModel.cs
@@ -29,93 +29,11 @@ namespace View3D.model
         private Coord3D position = new Coord3D();      // shift position
         private Coord3D rotation = new Coord3D();      // rotate vector
         private Coord3D scale = new Coord3D(1, 1, 1);  // scaler magnitude
-        public LinkedList<ModelAnimation> animations = new LinkedList<ModelAnimation>();
         public float xMin = 0, yMin = 0, zMin = 0, xMax = 0, yMax = 0, zMax = 0;              // min and max print region
         public Matrix4 curPos = Matrix4.Identity;  // position/rotation/scale
 
         // pre-rotate, diff-rotate?, pre-s?, pre-move?
         public float preRX = 0, preRY = 0, preRZ = 0, dRX = 0, dRY = 0, dRZ = 0, preSX = 0, preSY = 0, preSZ = 0, preMX = 0, preMY = 0, preMZ = 0, preRX2 = 0, preRY2 = 0, preRZ2 = 0;
-
-        public void addAnimation(ModelAnimation anim)
-        {
-            animations.AddLast(anim);
-        }
-
-        public void removeAnimationWithName(string aname)
-        {
-            bool found = true;
-            while (found)
-            {
-                found = false;
-                foreach (ModelAnimation a in animations)
-                {
-                    if (a.name.Equals(aname))
-                    {
-                        found = true;
-                        animations.Remove(a);
-                        break;
-                    }
-                }
-            }
-        }
-
-        public bool hasAnimationWithName(string aname)
-        {
-            foreach (ModelAnimation a in animations)
-            {
-                if (a.name.Equals(aname))
-                {
-                    return true;
-                }
-            }
-            return false;
-        }
-
-        public void clearAnimations()
-        {
-            animations.Clear();
-        }
-
-        public bool hasAnimations
-        {
-            get { return animations.Count > 0; }
-        }
-
-        public void AnimationBefore()
-        {
-            foreach (ModelAnimation a in animations)
-                a.BeforeAction(this);
-        }
-
-        /// <summary>
-        /// Plays the after action and removes finished animations.
-        /// </summary>
-        public void AnimationAfter()
-        {
-            bool remove = false;
-            foreach (ModelAnimation a in animations)
-            {
-                a.AfterAction(this);
-                remove |= a.AnimationFinished();
-            }
-            if (remove)
-            {
-                bool found = true;
-                while (found)
-                {
-                    found = false;
-                    foreach (ModelAnimation a in animations)
-                    {
-                        if (a.AnimationFinished())
-                        {
-                            found = true;
-                            animations.Remove(a);
-                            break;
-                        }
-                    }
-                }
-            }
-        }
 
         public bool Selected
         {
