191d5775aa79ab86c3369acff43abf9ad997f803
Charles Chang <42170907+CharlesVoyager@users.noreply.github.com>
2026-04-05 18:50:55 +0800

    Clean code.

--- ModelLib/Utils/RayCasting.cs
+++ ModelLib/Utils/RayCasting.cs
@@ -49,62 +49,6 @@ namespace OpenGL3DViewerNET10.ModelLib.Utils
             return new Ray(nearPos, Vector3.Normalize(farPos - nearPos));
         }
 
-        public static bool RaycastTriangle(Ray ray, Vector3[] triangle, out Vector3 p)
-        {
-            float t = -1;
-            bool result = RaycastTriangle(ray, triangle, out t);
-            p = ray.Position + ray.Normal * t;
-            return result;
-        }
-
-        private static bool RaycastTriangle(Ray ray, Vector3[] triangle, out float t)
-        {
-            if (!RayIntersectTriangle(ray, triangle, out t))
-            {
-                return false;
-            }
-
-            if (t > 0.00001)
-                return true;
-            else
-                return false;
-        }
-
-        private static bool RayIntersectTriangle(Ray ray, Vector3[] triangle, out float t)
-        {
-            t = -1;
-            Line ba = new Line(triangle[1], triangle[0]);
-            Line ca = new Line(triangle[2], triangle[0]);
-            Vector3 baVec = ba.ToVector();
-            Vector3 caVec = ca.ToVector();
-
-            Vector3 pVec = new Vector3();
-            Vector3 rayNorm = ray.Normal;
-            Vector3.Cross(ref rayNorm, ref caVec, out pVec);
-            float det = Vector3.Dot(baVec, pVec);
-
-            if (det > -0.00001 && det < 0.00001)
-                return false;
-
-            float invDet = 1 / det;
-            Line pv0 = new Line(ray.Position, triangle[0]);
-            Vector3 tVec = pv0.ToVector();
-            float u = invDet * Vector3.Dot(tVec, pVec);
-
-            if (u < 0.0 || u > 1.0)
-                return false;
-
-            Vector3 qVec = new Vector3();
-            Vector3.Cross(ref tVec, ref baVec, out qVec);
-            float v = invDet * Vector3.Dot(rayNorm, qVec);
-
-            if (v < 0.0 || u + v > 1.0)
-                return false;
-
-            t = -invDet * Vector3.Dot(caVec, qVec);
-            return true;
-        }
-
         public static bool RaycastAABB(Ray ray, PrintModel md)
         {
             if (md.BoundingBox.minPoint == null || md.BoundingBox.maxPoint == null)
