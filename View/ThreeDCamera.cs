d6148c3cdc4c7fb2e89c71dd4a068afd4583e1bb
Charles Chang <42170907+CharlesVoyager@users.noreply.github.com>
2026-04-30 20:55:21 +0800

    Clean code.

--- View/ThreeDCamera.cs
+++ View/ThreeDCamera.cs
@@ -228,9 +228,6 @@ namespace View3D.view
 
         public void FitBoundingBox(RHBoundingBox box)
         {
-            RHVector3 shift = new RHVector3(    -0.5 * SettingsService.Instance.Settings.PrintAreaWidth, 
-                                                -0.5 * SettingsService.Instance.Settings.PrintAreaDepth, 
-                                                -0.5 * SettingsService.Instance.Settings.PrintAreaHeight);
             viewCenter = box.Center.asVector3();
             Distance = defaultDistance;
             int loops = 5;
